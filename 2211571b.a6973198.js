63f0c8616536da066a582d0b67565cbea39705e4
xavier506 <xavier@crservers.com>
2020-11-20 07:06:49 +0000

    Deploying to gh-pages from  @ 264f150b54899a4a3a1613f7acff2cb483c078de 🚀

---
+++ 2211571b.a6973198.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{82:function(s){s.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"logistics","name":"logistics","count":1,"permalink":"/blog/tags/logistics"}')}}]);
