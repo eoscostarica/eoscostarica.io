f038fc0cf9a9aa81e05514db916323cb319b2c22
Jean <jean0798@gmail.com>
2021-02-02 08:27:34 -0600

    implement component on industries

--- src/pages/industries.js
+++ src/pages/industries.js
@@ -13,6 +13,7 @@ import AccordionDetails from '@material-ui/core/AccordionDetails';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import ScrollTrigger from 'react-scroll-trigger';
 
+import AdditionalResources from './components/AdditionalResources'
 import Payments from './Animations/Payments';
 import Logistic from './Animations/Logistic';
 import Games from './Animations/Games';
@@ -444,51 +445,6 @@ const Industries = () => {
               </Grid>
             </Grid>
           </Box>
-        </Box>
-      </Box>
-    )
-  }
-
-  const AdditionalResources = () => {
-    return (
-      <Box className={"containerSec"}>
-        <Box className={"section"}>
-          <Box className={"titleBox"}>
-            <h2>Additional Resources</h2>
-          </Box>
-          <p>
-            Find more interesting reads and resources to continue learning
-            about
-            <a href={"https://eoscostarica.medium.com/how-to-choose-an-enterprise-blockchainplatform-7c3665994ad6"}
-              target="_blank"
-              > enterprise blockchain</a> and
-              <a href={" https://eos.io/"} target="_blank"> EOSIO</a>.
-          </p>
-          <br/>
-          <Grid className={"topSpacingBoxIcons"} style={{justifyContent:'space-evenly'}} container>
-            <Grid onClick={() => history.push("/block-producer/")} className={"gridButton"} item xs={12} md={4}>
-              <img srcSet={useBaseUrl("img/blog.svg")}/>
-              <h3 className={"titleBlog"}>Blog</h3>
-            </Grid>
-            <Grid onClick = {() => location.href='https://guias.eoscostarica.io/'} className={"gridButton"} item xs={12} md={4}>
-              <img srcSet={useBaseUrl("img/press.svg")}/>
-              <h3 style={{marginTop:'30px', marginLeft:'20px'}}>Press</h3>
-            </Grid>
-          </Grid>
-          <br/>
-          <Grid className={"topSpacingBoxIcons"} style={{justifyContent:'space-evenly'}} container>
-            <Grid onClick = {() => location.href='https://guide.eoscostarica.io/'} className={"gridButton"} item xs={12} md={4}>
-              <img srcSet={useBaseUrl("img/decs.svg")}/>
-              <h3 className={"titleDevs1"}>Devs</h3>
-              <h3 className={"titleDevs2"}>Portal</h3>
-            </Grid>
-            <Grid onClick={() => location.href='https://github.com/eoscostarica'} className={"gridButton"} item xs={12} md={4}>
-              <img srcSet={useBaseUrl("img/github-ours.svg")}/>
-              <h3 className={"titleGit1"}>Our</h3>
-              <h3 className={"titleGit2"}>Github</h3>
-            </Grid>
-          </Grid>
-          <br/>
         </Box>
       </Box>
     )
