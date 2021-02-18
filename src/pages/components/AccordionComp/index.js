082b435a7403cb5c989adb168e0f40286912ca8b
AngeloCG97 <angelocas13_8@hotmail.com>
2021-02-18 14:00:42 -0600

    update(frontend): add lines when accordions is expanded

--- src/pages/components/AccordionComp/index.js
+++ src/pages/components/AccordionComp/index.js
@@ -8,24 +8,24 @@ import ExpandMoreIcon from '@material-ui/icons/ExpandMore'
 const AccordionComp = ( {data, handleChange, expanded, bgColor} ) => {
   return (
     <>
-        {data.map((item) => (
-            <Accordion 
-                key={item.target}
-                square
-                expanded={expanded === item.target}
-                style={{backgroundColor:bgColor,boxShadow:'none'}}
-                onChange={handleChange(item.target)}
-                className="accordion">
-                <AccordionSummary style={{padding:1}} expandIcon={<ExpandMoreIcon style={{color:'#5484B3'}}/>}>
-                <h4 style={{margin:'2px', padding: 0}}>{item.title}</h4>
-                </AccordionSummary>
-                <AccordionDetails style={{padding:1, marginTop:'-15px'}}>
-                <p style={{padding: 0}}>
-                    {item.content}
-                </p>
-                </AccordionDetails>
-            </Accordion>
-        ))}
+      {data.map((item) => (
+        <Accordion 
+          key={item.target}
+          square
+          expanded={expanded === item.target}
+          style={{backgroundColor:bgColor, boxShadow:'none'}}
+          onChange={handleChange(item.target)}
+          className="accordion">
+          <AccordionSummary style={{padding:1}} expandIcon={<ExpandMoreIcon style={{color:'#5484B3'}}/>}>
+          <h4 style={{margin:'2px', padding: 0}}>{item.title}</h4>
+          </AccordionSummary>
+          <AccordionDetails style={{padding:1, marginTop:'-15px'}}>
+          <p style={{padding: 0}}>
+              {item.content}
+          </p>
+          </AccordionDetails>
+        </Accordion>
+      ))}
     </>
   );
 };
