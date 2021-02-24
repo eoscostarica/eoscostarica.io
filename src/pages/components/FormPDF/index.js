368177c34db20e7162f754ceef01bae67b5fb95c
Jean <jean0798@gmail.com>
2021-02-24 16:26:06 -0600

    temp preview

--- src/pages/components/FormPDF/index.js
+++ src/pages/components/FormPDF/index.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react"
 import PropTypes from 'prop-types'
-import {Page } from '@react-pdf/renderer'
+//import {Page } from '@react-pdf/renderer'
 import useBaseUrl from "@docusaurus/useBaseUrl"
 /*
 const styles = StyleSheet.create({
