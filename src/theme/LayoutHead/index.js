486e1e3da29faee3b06be6b2efa834d8e87a9686
AngeloCG97 <angelocas13_8@hotmail.com>
2021-02-25 09:18:35 -0600

    add(structured data): add LocalBussines structured data

--- src/theme/LayoutHead/index.js
+++ src/theme/LayoutHead/index.js
@@ -60,5 +60,68 @@ export default function LayoutHead(props) {
     >
         {metadatas.map((metadata, i) => <meta key={`metadata_${i}`} {...metadata} />)}
       </Head>
+      <html>
+        <head>
+          <script type="application/ld+json">
+            {`{
+              "@context": "https://schema.org",
+              "@type": "LocalBusiness",
+              "image": [
+                "https://raw.githubusercontent.com/eoscostarica/eoscostarica.io/master/static/img/metaImgBlack.png"
+               ],
+              "name": "EOS Costa Rica",
+              "address": {
+                "@type": "PostalAddress",
+                "streetAddress": "148 W 51st St",
+                "addressLocality": "Costa Rica",
+                "addressRegion": "NY",
+                "postalCode": "10019",
+                "addressCountry": "CR"
+              },
+              "geo": {
+                "@type": "GeoCoordinates",
+                "latitude": 40.761293,
+                "longitude": -73.982294
+              },
+              "url": "https://eoscostarica.io/",
+              "telephone": "+12122459600",
+              "priceRange": "$$$",
+              "openingHoursSpecification": [
+                {
+                  "@type": "OpeningHoursSpecification",
+                  "dayOfWeek": [
+                    "Monday",
+                    "Tuesday"
+                  ],
+                  "opens": "11:30",
+                  "closes": "22:00"
+                },
+                {
+                  "@type": "OpeningHoursSpecification",
+                  "dayOfWeek": [
+                    "Wednesday",
+                    "Thursday",
+                    "Friday"
+                  ],
+                  "opens": "11:30",
+                  "closes": "23:00"
+                },
+                {
+                  "@type": "OpeningHoursSpecification",
+                  "dayOfWeek": "Saturday",
+                  "opens": "16:00",
+                  "closes": "23:00"
+                },
+                {
+                  "@type": "OpeningHoursSpecification",
+                  "dayOfWeek": "Sunday",
+                  "opens": "16:00",
+                  "closes": "22:00"
+                }
+              ]
+            }`}
+          </script>
+        </head>
+      </html>
     </>;
 }
