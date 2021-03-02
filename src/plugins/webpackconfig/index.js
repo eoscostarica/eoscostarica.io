498fe90eb1373d43934617bc0558832540bdd4b7
AngeloCG97 <angelocas13_8@hotmail.com>
2021-03-02 14:36:42 -0600

    fix(node.yml): add --no-minify command

--- src/plugins/webpackconfig/index.js
+++ src/plugins/webpackconfig/index.js
@@ -9,10 +9,6 @@ module.exports = function () {
             { 
                 test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
                 loader: 'url-loader?limit=100000'
-            },
-            {
-                test: /\.css$/i,
-                use: ["style-loader", "css-loader"]
             }
           ],
         },
