343c065a4bba5caae62c3422da5b73b197f5c0c2
AngeloCG97 <angelocas13_8@hotmail.com>
2021-03-02 11:00:23 -0600

    fix(font import): Change import font by local import

--- src/plugins/webpackconfig/index.js
+++ src/plugins/webpackconfig/index.js
@@ -6,9 +6,14 @@ module.exports = function () {
       return {
         module: {
           rules: [
-          { 
-              test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
-              loader: 'url-loader?limit=100000' }
+            { 
+                test: /\.(png|woff|woff2|eot|ttf|svg)$/, 
+                loader: 'url-loader?limit=100000'
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"]
+            }
           ],
         },
         plugins: [
