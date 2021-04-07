1082bcc23c15ca8907d253f3ed2c9114b3677ef8
AngeloCG97 <angelocas13_8@hotmail.com>
2021-04-07 15:36:22 -0600

    fix(aditionalResources.js): aline buttons in movile version

--- src/pages/components/AdditionalResources/index.js
+++ src/pages/components/AdditionalResources/index.js
@@ -4,11 +4,9 @@ import Box from '@material-ui/core/Box'
 import { useHistory } from 'react-router-dom'
 import Grid from '@material-ui/core/Grid'
 import useBaseUrl from "@docusaurus/useBaseUrl"
-import { useMediaQuery } from 'react-responsive'
 
 const AdditionalResources = () => {
   const history = useHistory()
-  const isMobile = useMediaQuery( {query:'(max-width: 960px)'} )
   return (
     <Box className="containerSec">
       <Box className="section">
@@ -26,31 +24,31 @@ const AdditionalResources = () => {
         </Box>
         <br/>
         <Grid container spacing={5} justify="center" >
-          <Grid onClick={() => history.push("/blog/")} className="gridButton" item xs={12} md={5}>
+          <Grid onClick={() => history.push("/blog/")} className="gridButton" item xs={5} md={5}>
             <Box className={clsx("centerBox", "spacingBox", "paddingLeft")}>
               <img className="resourcesIcon" srcSet={useBaseUrl("img/icons/blog.svg")}/>
-              <h3 style={{marginLeft:'25px', width:'210px'}}>Blog</h3>
+              <h3 className="marginLeft" style={{ width:'210px'}}>Blog</h3>
             </Box>
           </Grid>
-          <Grid onClick = {() => history.push("/press/")} className="gridButton" item xs={12} md={5}>
+          <Grid onClick = {() => history.push("/press/")} className="gridButton" item xs={5} md={5}>
             <Box className={clsx("centerBox", "spacingBox", "paddingLeft")}>
               <img className="resourcesIcon" srcSet={useBaseUrl("img/icons/press.svg")}/>
-              <h3 style={{marginLeft:'25px', width:'210px'}}>Press</h3>
+              <h3 className="marginLeft" style={{ width:'210px'}}>Press</h3>
             </Box>
           </Grid>
-          <Grid onClick = {() => window.open('https://guide.eoscostarica.io/')} className="gridButton" item xs={12} md={5}>
-            <Box className={clsx("centerBox", "spacingBox", "paddingLeft2")}>
+          <Grid onClick = {() => window.open('https://guide.eoscostarica.io/')} className="gridButton" item xs={5} md={5}>
+            <Box className={clsx("centerBox", "paddingLeft2")}>
               <img className="resourcesIcon" srcSet={useBaseUrl("img/icons/decs.svg")}/>
-              <Box style={{marginLeft:'25px'}}>
-                <h3 style={{marginBottom:'5px', width:'210px'}}>Devs </h3>
-                <h3 style={{marginBottom:'5px', width:'210px'}}>Portal</h3>
+              <Box className="marginLeft">
+                <h3 style={{ width:'210px'}}>Devs </h3>
+                <h3 style={{ width:'210px'}}>Portal</h3>
               </Box>
             </Box>
           </Grid>
-          <Grid onClick={() => window.open('https://github.com/eoscostarica')} className="gridButton" item xs={12} md={5}>
-          <Box className={clsx("centerBox", "spacingBox", "paddingLeft2")}>
+          <Grid onClick={() => window.open('https://github.com/eoscostarica')} className="gridButton" item xs={5} md={5}>
+          <Box className={clsx("centerBox", "paddingLeft2")}>
               <img className="resourcesIcon" srcSet={useBaseUrl("img/icons/github-ours.svg")}/>
-              <Box style={{marginLeft:'25px'}}>
+              <Box className="marginLeft">
                 <h3 style={{marginBottom:'5px', width:'210px'}}>Our</h3>
                 <h3 style={{marginBottom:'5px', width:'210px'}}>Github</h3>
               </Box>
