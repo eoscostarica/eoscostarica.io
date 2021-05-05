07bc730578164d6a2fab394c6b35f77a31a7c359
AngeloCG97 <angelocas13_8@hotmail.com>
2021-05-05 10:51:26 -0600

    test deploy

--- src/plugins/webpackconfig/index.js
+++ src/plugins/webpackconfig/index.js
@@ -1,5 +1,4 @@
 const webpack = require('webpack')
-const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 module.exports = function () {
   return {
     name: 'custom-docusaurus-plugin',
@@ -16,8 +15,7 @@ module.exports = function () {
         plugins: [
           new webpack.ProvidePlugin({
               Buffer: ['buffer', 'Buffer'],
-          },
-          new NodePolyfillPlugin()
+          }
           )
         ],
       };
